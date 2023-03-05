33787b17975d4825b716556c44cd56164976df3b
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-03-05 14:44:13 +0000

    Began working on step 3, semi complete step 2.

---
+++ src/InitialiseDB.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+
+public class InitialiseDB {
+    public static void main(String[] args) {
+        File file = new File("CS1003_P3DataBase");
+        if (file.exists()) {
+            file.delete();
+        }
+        file = new File(file.getName());
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = null;
+        try {
+            String path = "jdbc:sqlite:" + file.getName();
+            connection = DriverManager.getConnection(path);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS popStars");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS popStars(Name VARCHAR(100) NOT NULL PRIMARY KEY, Label VARCHAR(100) NOT NULL, HighestNum INTEGER)");
+            statement.close();
+            statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM popStars");
+            System.out.println("\ncontents of table:");
+            while (result.next()) {
+                String name = result.getString(1);
+                String label = result.getString(2);
+                int highest = result.getInt("HighestNum");
+                System.out.println("name: " + name);
+                System.out.println("label: " + label);
+                System.out.println("highest: " + highest);
+                System.out.println();
+            }
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
